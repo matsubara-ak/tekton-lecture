42cf0b007ad7852c397d802b9bdf462addcda4ef
Matsubara Akimitsu <akimitsu@fujitsu.com>
2021-12-16 19:07:13 +0900

    update

--- 2/src/app/main.py
+++ 2/src/app/main.py
@@ -1,14 +1,13 @@
-from flask import Flask, request
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-    return 'Root Page!'
+@app.route("/")
+def index():
+  return "Tekton lecture!"
 
-@app.route('/hello')
-def hello():
-    return 'Hello World!'
+@app.route("/hello")
+def index():
+  return "Hello World!"
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=8080, debug=True)
